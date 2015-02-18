9b9e0876dece93e03ebed2c09a4aae053555f0fb
Ryan Burke <ryanb-93@hotmail.co.uk>
2015-02-18 12:52:11 +0000

    Fixed memory allocation bug on NSMutableArray.

--- CertManager/FSHandler.m
+++ CertManager/FSHandler.m
@@ -10,9 +10,17 @@
 
 @implementation FSHandler
 
+/**
+ *  Returns the path of a plist from within the documents directory.
+ *  If the file does not exist already then it will create it.
+ *
+ *  @param fileName The name of the plist.
+ *
+ *  @return The full path of the plist.
+ */
 + (NSString *) getFilePathForFileName:(NSString *)fileName {
     
-    NSString *filePath = [NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES) lastObject];
+    NSString *filePath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
     filePath = [filePath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.plist", fileName]];
     
     NSFileManager *fileManager = [NSFileManager defaultManager];
@@ -24,17 +32,29 @@
     return filePath;
 }
 
-
+/**
+ *  Writes an array to the plist.
+ *
+ *  @param fileName The name of the plist to write to.
+ *  @param data     The data to write to the file.
+ */
 + (void) writeToPlist: (NSString*)fileName withData:(NSMutableArray *)data
 {
     NSString *filePath = [self getFilePathForFileName:fileName];
     [data writeToFile:filePath atomically: YES];
 }
 
+/**
+ *  Reads an array from a plist.
+ *
+ *  @param fileName The name of the plist to read from.
+ *
+ *  @return The values of the plist in an array.
+ */
 + (NSMutableArray *) readFromPlist: (NSString *)fileName {
     NSString *filePath = [self getFilePathForFileName:fileName];
-    NSArray *arr = [[NSMutableArray alloc] initWithContentsOfFile:filePath];
-    return [NSMutableArray arrayWithArray:arr];
+    NSArray *arr = [[NSArray alloc] initWithContentsOfFile:filePath];
+    return [[NSMutableArray alloc] initWithArray:arr];
 }
 
 @end
